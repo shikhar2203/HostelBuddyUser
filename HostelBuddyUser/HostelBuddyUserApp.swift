e77be94ef923270fe22ce444dad95f7efaab23e5
shikhar2203 <shikharsharma2203@gmail.com>
2024-03-19 18:28:24 +0530

    Initial Commit

---
+++ HostelBuddyUser/HostelBuddyUserApp.swift
@@ -0,0 +1,17 @@
+//
+//  HostelBuddyUserApp.swift
+//  HostelBuddyUser
+//
+//  Created by shikhar on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HostelBuddyUserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
